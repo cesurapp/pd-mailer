4d6157d51b983c93c46269374252d86f6c906086
Kerem APAYDIN <apaydin541@gmail.com>
2019-03-16 16:04:50 +0300

    Fixed Deprecated

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -23,7 +23,7 @@ class Configuration implements ConfigurationInterface
 {
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
+        $treeBuilder = new TreeBuilder('pd_mailer');
         $rootNode = $treeBuilder->root('pd_mailer');
 
         // Set Configuration
